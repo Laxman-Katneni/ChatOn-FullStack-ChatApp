9a49d8c3ae6a4d719476e1bbc65978732e520db4
Laxman Katneni <143445097+Laxman-Katneni@users.noreply.github.com>
2025-07-31 19:18:09 -0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,16 +10,13 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import fs from "fs";
 
-
 import path from "path"; // For Deployment
 dotenv.config();
 
 //const app = express(); Removing as created one in socket.js
 
 const PORT = process.env.PORT;
-//const __dirname = path.resolve(); Error while deploying
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.resolve();
 
 //app.use(express.json()); // Middleware to allow extracting the json data from the body
 // PayloadTooLargeError: request entity too large // as by default, max limit is 100kb, which is kinda small for images
@@ -33,24 +30,12 @@ app.use(
     credentials: true, // To allow cookies or authorization headers
   })
 );
-try {
-  console.log("⏳ Mounting /api/auth");
-  app.use("/api/auth", authRoutes);
-  console.log("✅ Mounted /api/auth");
-} catch (err) {
-  console.error("🔥 Error mounting /api/auth", err);
-}
 
-try {
-  console.log("⏳ Mounting /api/messages");
-  app.use("/api/messages", messageRoutes);
-  console.log("✅ Mounted /api/messages");
-} catch (err) {
-  console.error("🔥 Error mounting /api/messages", err);
-}
+app.use("/api/auth", authRoutes); // Authentication
+app.use("/api/messages", messageRoutes); // Messages
 
 // If we are in production, go ahead and make the dist folder our static assets/ using static middleware from express
-/*
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -59,27 +44,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     // serving both api and the react application under the same place
   });
-}*/
-if (process.env.NODE_ENV === "production") {
-  const clientPath = path.join(__dirname, "../client-build");
-  console.log("⚡ Serving React from:", clientPath);
-
-  app.use(express.static(clientPath));
-
-  app.get("*", (req, res) => {
-    const indexFile = path.join(clientPath, "index.html");
-
-    if (fs.existsSync(indexFile)) {
-      res.sendFile(indexFile);
-    } else {
-      console.error("❌ index.html does not exist at:", indexFile);
-      res.status(500).send("index.html not found.");
-    }
-  });
 }
-
-
-
 
 server.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
